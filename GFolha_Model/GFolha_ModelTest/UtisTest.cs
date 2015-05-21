cbbed2ee2df40285ba206f8ec37779ee451088c3
ivensleao <ivensleao@gmail.com>
2015-05-21 17:50:34 -0300

    Inserindo testes unitarios

---
+++ GFolha_Model/GFolha_ModelTest/UtisTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GFolha_Model.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GFolha_ModelTest
+{
+    [TestClass]
+    public class UtisTest
+    {
+        [TestMethod]
+        public void TesteObjetoNull()
+        {
+            //Assert.AreNotEqual(Utils.CheckNullObject(null), true);
+        }
+
+        [TestMethod]
+        public void TesteObjetoPreenchido()
+        {
+            var obj = new object();
+
+            Assert.AreEqual(Utils.CheckNullObject(obj), false);
+        }
+    }
+}
